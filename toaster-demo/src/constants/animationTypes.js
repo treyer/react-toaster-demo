4c052054b9405f9ff5401430226476f6f33d58d7
treyer <nemereno_by@mail.ru>
2022-11-10 20:03:39 +0300

    refactor: update toaster lib

--- toaster-demo/src/constants/animationTypes.js
+++ toaster-demo/src/constants/animationTypes.js
@@ -1,13 +1,10 @@
 export const ANIMATION_TYPES = {
-  EASE: 'ease',
-  EASE_IN: 'ease-in',
-  EASE_OUT: 'ease-out',
-  EASE_IN_OUT: 'ease-in-out',
-  LINEAR: 'linear',
-  STEP_START: 'step-start',
-  STEP_END: 'step-end',
-  CUBIC_BEZIER: 'cubic-bezier',
-}
+  EASE: "ease",
+  EASE_IN: "ease-in",
+  EASE_OUT: "ease-out",
+  EASE_IN_OUT: "ease-in-out",
+  LINEAR: "linear",
+  CUBIC_BEZIER: "cubic-bezier",
+};
 
-export const CUBIC_BEZIER_FN =
-  'cubic-bezier(0.1, 0.7, 1, 0.1)'
+export const CUBIC_BEZIER_FN = "cubic-bezier(0.1, 0.7, 1, 0.1)";
